cef8a647dc2a049a42e5a618b2ee3b182e1d8405
zhangyy <i@zhangtory.com>
2020-11-05 11:31:05 +0800

    update

---
+++ core-spring-boot/core-spring-boot-autoconfigure/src/main/java/com/zhangtory/core/CoreSpringBootAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.zhangtory.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSpringBootAutoconfigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreSpringBootAutoconfigureApplication.class, args);
+    }
+
+}
---
+++ jwt-spring-boot/jwt-spring-boot-autoconfigure/src/test/java/com/zhangtory/jwt/JwtSpringBootAutoconfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangtory.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringBootAutoconfigureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
